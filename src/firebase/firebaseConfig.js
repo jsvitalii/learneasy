6181bcb047615f1ac0c4e6f10136311c969e774e
vitalii <bvitaly97@gmail.com>
2023-10-15 19:46:05 +0300

    fix: prettier

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-   apiKey: "AIzaSyAo2NQMyp3vpZUsiNVAVDYfOA96-2kKQBU",
-   authDomain: "learneasy-2be75.firebaseapp.com",
-   projectId: "learneasy-2be75",
-   storageBucket: "learneasy-2be75.appspot.com",
-   messagingSenderId: "437833798963",
-   appId: "1:437833798963:web:3ed5605ab03dbe77da5a46",
-   measurementId: "G-HR5084NEQ4"
+  apiKey: 'AIzaSyAo2NQMyp3vpZUsiNVAVDYfOA96-2kKQBU',
+  authDomain: 'learneasy-2be75.firebaseapp.com',
+  projectId: 'learneasy-2be75',
+  storageBucket: 'learneasy-2be75.appspot.com',
+  messagingSenderId: '437833798963',
+  appId: '1:437833798963:web:3ed5605ab03dbe77da5a46',
+  measurementId: 'G-HR5084NEQ4',
 };
 
 export default firebaseConfig;
